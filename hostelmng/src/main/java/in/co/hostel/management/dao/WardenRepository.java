0eef2c28ca5f89efa0dd4751b0dfcab2456b6dc9
Tushar Mall <115815953+tusharmall@users.noreply.github.com>
2024-10-14 16:32:08 +0530

    Delete hostelmng directory

--- hostelmng/src/main/java/in/co/hostel/management/dao/WardenRepository.java
+++
@@ -1,7 +0,0 @@
-package in.co.hostel.management.dao;
-
-import in.co.hostel.management.entity.Warden;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WardenRepository extends JpaRepository<Warden, Long> {
-}
